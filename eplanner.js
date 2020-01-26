6622f9affd80f3f84f7c799a5bc4dcc6f91e1128
IILMTII <55761853+IILMTII@users.noreply.github.com>
2020-01-26 00:20:26 -0600

    Add files via upload

--- eplanner.js
+++ eplanner.js
@@ -1,1 +1,21 @@
-fd
+var table_body;
+$(document).ready(function(){
+    for(var i=0;i<9;i++){
+            table_body+='<tr class="d-flex">';
+        
+            table_body +='<td class="col-sm-1">';
+            table_body +='Table data';
+            table_body +='</td>';
+        
+            table_body +='<td class="col-sm-10">';
+            table_body +='<textarea class="form-control" rows="2" id="comment"></textarea>';
+            table_body +='</td>';
+        
+            table_body +='<td class="col-sm-1">';
+            table_body +='<i class="fa fa-save" style="font-size:36px;"></i>';
+            table_body +='</td>';
+
+            table_body+='</tr>';
+        }
+        $('tbody').append(table_body);
+});
